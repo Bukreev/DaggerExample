0b4e1e0ba6bee849d2db9ebf66a85a1c6a88409c
Evgeniy Bukreev <suzuka1100@mail.ru>
2019-05-02 15:52:04 +0300

    Добавил ButterKnife

---
+++ app/src/main/java/com/bukreevei/daggerexample/view/input/InputDataView.java
@@ -0,0 +1,13 @@
+package com.bukreevei.daggerexample.view.input;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+public class InputDataView extends LinearLayout {
+    public InputDataView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+
+    }
+}
